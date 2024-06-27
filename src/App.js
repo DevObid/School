3c152331acafbc60dddeba44fe36d8b8c7ed52d1
obid <mehdiobidobid@gmail.com>
2024-06-27 03:47:28 -0700

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Sidebar/Sidebar';
 import Navbar from './Navbar/Navbar';
+import Shart from './Sharts/Shart';
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
       <Navbar />
+      <Shart />
     </div>
   );
 }
