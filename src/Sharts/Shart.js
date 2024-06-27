d52d30d08abffbb9dc063454ca5f699a4c7d46b8
obid <mehdiobidobid@gmail.com>
2024-06-27 04:55:47 -0700

    po

--- src/Sharts/Shart.js
+++ src/Sharts/Shart.js
@@ -377,8 +377,9 @@ function Shart() {
                 </div>
             </div>
 
-            <div className="  absolute right-[10px] rounded-md mt-[90px] shadow-md  ">
-                <table className=" w-[750px]  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <div class=" absolute right-[10px] w-[750px] mt-[100px]  overflow-x-auto shadow-md sm:rounded-lg">
+
+                <table className=" w-[750px]   text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead style={{ backgroundColor: "#1C64F2" }} className="text-xs text-white  uppercase  dark:bg-gray-700 dark:text-gray-400">
                         <tr >
                             <th scope="col" className="px-6 py-3">Student Name</th>
@@ -389,17 +390,23 @@ function Shart() {
                     </thead>
                     <tbody>
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
+                            <th scope="row" className="px-6 py-4  font-medium flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
                             </th>
-                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">100</td>
                             <td className="px-6 py-4">Laptop</td>
                             <td className="px-6 py-4">$2999</td>
 
                         </tr>
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
                             </th>
                             <td className="px-6 py-4">White</td>
                             <td className="px-6 py-4">Laptop PC</td>
@@ -408,7 +415,10 @@ function Shart() {
                         </tr>
                         <tr className="bg-white dark:bg-gray-800">
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
                             </th>
                             <td className="px-6 py-4">Black</td>
                             <td className="px-6 py-4">Accessories</td>
@@ -417,6 +427,162 @@ function Shart() {
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+            <div class=" absolute right-[10px] w-[750px] mt-[350px] h-[240px]  overflow-x-auto shadow-md sm:rounded-lg">
+
+                <table className=" w-[750px] h-[240px]   text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead style={{ backgroundColor: "#1C64F2" }} className="text-xs text-white  uppercase  dark:bg-gray-700 dark:text-gray-400">
+                        <tr >
+                            <th scope="col" className="px-6 py-3">Student Name</th>
+                            <th scope="col" className="px-6 py-3">class</th>
+                            <th scope="col" className="px-6 py-3">age</th>
+                            <th scope="col" className="px-6 py-3">full name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4  font-medium flex items-center text-gray-900 whitespace-nowrap dark:text-white">
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
+                            </th>
+                            <td className="px-6 py-4">100</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
+                            </th>
+                            <td className="px-6 py-4">White</td>
+                            <td className="px-6 py-4">Laptop PC</td>
+                            <td className="px-6 py-4">$1999</td>
+
+                        </tr>
+                        <tr className="bg-white dark:bg-gray-800">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <div className=" flex items-center">
+                                    <img className="w-8 h-8 p-1 absolute  rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="favicon.png" alt="Bordered avatar" />
+                                    <span className="ml-16">EL MEHDI OBID</span>
+                                </div>
+                            </th>
+                            <td className="px-6 py-4">Black</td>
+                            <td className="px-6 py-4">Accessories</td>
+                            <td className="px-6 py-4">$99</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div className="bg-slate-500 w-[1070px] h-96 absolute top-[700px] right-[10px]">
+
+                <div className="flex justify-between mb-3">
+                    <div className="flex justify-center items-center">
+                        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">
+                            Website traffic
+                        </h5>
+                        <svg
+                            data-popover-target="chart-info"
+                            data-popover-placement="bottom"
+                            className="w-3.5 h-3.5 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white cursor-pointer ms-1"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm0 16a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm1-5.034V12a1 1 0 0 1-2 0v-1.418a1 1 0 0 1 1.038-.999 1.436 1.436 0 0 0 1.488-1.441 1.501 1.501 0 1 0-3-.116.986.986 0 0 1-1.037.961 1 1 0 0 1-.96-1.037A3.5 3.5 0 1 1 11 11.466Z" />
+                        </svg>
+                        <div
+                            data-popover=""
+                            id="chart-info"
+                            role="tooltip"
+                            className="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400"
+                        >
+                            <div className="p-3 space-y-2">
+                                <h3 className="font-semibold text-gray-900 dark:text-white">
+                                    Activity growth - Incremental
+                                </h3>
+                                <p>
+                                    Report helps navigate cumulative growth of community activities.
+                                    Ideally, the chart should have a growing trend, as stagnating chart
+                                    signifies a significant decrease of community activity.
+                                </p>
+                                <h3 className="font-semibold text-gray-900 dark:text-white">
+                                    Calculation
+                                </h3>
+                                <p>
+                                    For each date bucket, the all-time volume of activities is
+                                    calculated. This means that activities in period n contain all
+                                    activities up to period n, plus the activities generated by your
+                                    community in period.
+                                </p>
+                                <a
+                                    href="#"
+                                    className="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:underline"
+                                >
+                                    Read more{" "}
+                                    <svg
+                                        className="w-2 h-2 ms-1.5 rtl:rotate-180"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 6 10"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth={2}
+                                            d="m1 9 4-4-4-4"
+                                        />
+                                    </svg>
+                                </a>
+                            </div>
+                            <div data-popper-arrow="" />
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            type="button"
+                            data-tooltip-target="data-tooltip"
+                            data-tooltip-placement="bottom"
+                            className="hidden sm:inline-flex items-center justify-center text-gray-500 w-8 h-8 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm"
+                        >
+                            <svg
+                                className="w-3.5 h-3.5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 16 18"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"
+                                />
+                            </svg>
+                            <span className="sr-only">Download data</span>
+                        </button>
+                        <div
+                            id="data-tooltip"
+                            role="tooltip"
+                            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                        >
+                            Download CSV
+                            <div className="tooltip-arrow" data-popper-arrow="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
